478f8ed9d12dc83f7275d87b8b2ae2314e13215d
kbmgs <kbmgs00@gmail.com>
2022-03-30 23:09:53 +0800

    20220330-开发环境wordcount(视频章节7-10)

--- spark-core/src/main/java/com/atguigu/bigdata/spark/core/wc/Spark03_WordCount.scala
+++ spark-core/src/main/java/com/atguigu/bigdata/spark/core/wc/Spark03_WordCount.scala
@@ -3,7 +3,7 @@ package com.atguigu.bigdata.spark.core.wc
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark01_WordCount {
+object Spark03_WordCount {
   def main(args: Array[String]): Unit = {
     //Application
     //Spark框架
@@ -14,31 +14,20 @@ object Spark01_WordCount {
     val sc = new SparkContext(sparConf) //增加和spark的依赖关系 或者 提供jar包
     //TODO 执行业务操作
 
-    //1.读取文件，获取一行一行的数据
-    //hello world
+
     val lines: RDD[String] = sc.textFile("datas")
 
-    //2.将一行数据进行拆分，形成一个一个的单词(分词) --- 扁平化操作
-    //扁平化：将整体拆分成个体的操作
-    //"hello world"... => hello,world,hello,world
     val words: RDD[String] = lines.flatMap(_.split(" "))
 
-    //3.将数据根据单词进行分组，便于统计
-    //(hello,hello,hello),(world,world)
-    val wordGroup: RDD[(String, Iterable[String])] = words.groupBy(word => word) //Iterable可迭代的
-
-    //    println(wordGroup)
-    //4.对分组后的数据进行聚合
-    //(hello,3),(world,2)
-    val wordToCount = wordGroup.map {
-      case (word, list) => {
-        (word, list.size)
-      }
-    }
+    //优化算法，增加预聚合 --- hello,hello,world => (hello,1)(hello,1)(world,1)
+    val wordToOne: RDD[(String, Int)] = words.map(
+      word => (word, 1)   //.map(_,1)
+    )
+    //Spark框架提供了更多的功能。可以将分组和聚合使用一个方法实现。
+    //reduceByKey：相同的key的数据，可以对value进行reduce聚合
+    val wordToCount = wordToOne.reduceByKey((x, y) => x + y) //.reduceByKey(_+_)
 
     //5.将转换结果采集到控制台打印出来
-    wordGroup.collect().foreach(println)
-    println("**********************")
     val array: Array[(String, Int)] = wordToCount.collect()
     array.foreach(println)
 
